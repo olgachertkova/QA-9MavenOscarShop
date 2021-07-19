fa4d06d339120e754871a4075e00974dc0b178eb
olgachertkova <olga@chertkov.info>
2021-07-19 19:22:57 +0300

    first commit

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    WebDriver driver;
+    @BeforeMethod
+    public void openBrowser(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://selenium1py.pythonanywhere.com/en-gb/");
+    }
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+}
